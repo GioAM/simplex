3eb58e269bb3ae036c221a0d83cf6a2eae91888d
Leticia P. May <43357500+leticiapmay99@users.noreply.github.com>
2020-11-23 19:21:58 -0300

    type number

--- js/custom.js
+++ js/custom.js
@@ -42,7 +42,7 @@ function montarEstrutura(){
 			$("#funcao").append(`
 				<div class='col-md-${divisao} mb-3'>
 	                <label for='firstName'>x${i}</label>
-	                <input type="text" class="form-control" id="x${i}" placeholder="" value="">
+	                <input type="number" class="form-control" id="x${i}" placeholder="" value="">
 	            </div>`);
 		}
 		var divisao = 2;
@@ -56,13 +56,13 @@ function montarEstrutura(){
 				$("#restricao_" + i).append(`
 					<div class='col-md-${divisao} mb-3'>
 		                <label for='firstName'>x${j}</label>
-		                <input type="text" class="form-control" id="restricao_${i}_x${j}" placeholder="" value="">
+		                <input type="number" class="form-control" id="restricao_${i}_x${j}" placeholder="" value="">
 		            </div>`);
 			}
 			$("#restricao_" + i).append(`
 				<div class='col-md-${divisao} mb-3'>
 	                <label for='firstName'>b</label>
-	                <= <input type="text" class="form-control" id="restricao_${i}_b" placeholder="" value="">
+	                <= <input type="number" class="form-control" id="restricao_${i}_b" placeholder="" value="">
 	            </div>`);
 		}
 		atualizar_labels();
@@ -258,7 +258,14 @@ function informacoes(dados){
 	if(tem_negativos(dados[0])){
 		$(`#informacoes_${iteracoes} tbody`).append(`<tr class="solucao"><th scope="row"> Solução não ótima </th></tr>`);
 	}else{
-		$(`#informacoes_${iteracoes} tbody`).append(`<tr class="solucao"><th scope="row"> Solução ótima </th></tr>`);
+		$(`#informacoes_${iteracoes} tbody`).append(`<tr class="solucao"><th scope="row"> Solução ótima <span>
+			<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-info-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+				<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+				<path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/>
+				<circle cx="8" cy="4.5" r="1"/>
+			</svg>
+		</span>
+		</th></tr>`);
 	}
 	$(`#informacoes_${iteracoes} tbody .solucao`).append(`<td>Z = ${dados[0][size-1]}</td>`);
 }
